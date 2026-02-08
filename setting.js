d18d086de8de89d0570f683e44f858e9ef1a671d
asaddjhein <117557295+asaddjhein@users.noreply.github.com>
2026-02-08 07:49:58 +0500

    Add files via upload

---
+++ setting.js
@@ -0,0 +1,16 @@
+const settings = {
+  packname: '𓆩 𝘒𝘏𝘈𝘕-𝘔𝘋 𓆪',
+  author: '‎',
+  botName: "𓆩 𝘒𝘏𝘈𝘕-𝘔𝘋 𓆪",
+  botOwner: 'S𝘒𝘏𝘈𝘕1', // Your name
+  ownerNumber: '923267613661', //Set your number here without + symbol, just add country code & number without any space
+  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+  commandMode: "public",
+  maxStoreMessages: 20, 
+  storeWriteInterval: 10000,
+  description: "This is a bot for managing group commands and automating tasks.",
+  version: "3.0.6",
+  updateZipUrl: "https://github.com/asaddjhein/Bot",
+};
+
+module.exports = settings;
